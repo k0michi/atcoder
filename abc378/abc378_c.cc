3f38bbcf34c6ba086d9b26ab722ea84a8ecfe805
k0michi <k0michi@koyomi.co>
2024-11-17 17:37:21 +0900

    2024-11-17#3

---
+++ abc378/abc378_c.cc
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <set>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std::literals;
+
+int main() {
+  i64 N;
+  std::cin >> N;
+  std::vector<i64> A(N);
+  for (i64 i = 0; i < A.size(); i++) {
+    std::cin >> A[i];
+  }
+
+  std::unordered_map<i64, i64> appearances;
+
+  for (i64 i = 0; i < A.size(); i++) {
+    i64 B_i;
+
+    if (appearances.count(A[i]) > 0) {
+      B_i = appearances[A[i]] + 1;
+    } else {
+      B_i = -1;
+    }
+
+    if (i > 0) {
+      std::cout << ' ';
+    }
+
+    std::cout << B_i;
+    appearances[A[i]] = i;
+  }
+
+  std::cout << std::endl;
+exit:
+  return 0;
+}
