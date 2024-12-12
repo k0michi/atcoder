2345fb89eec1054b7013f9d077a0918eea89fdd3
k0michi <k0michi@koyomi.co>
2024-12-13 00:19:09 +0900

    2024-12-13

---
+++ abc103/abc103_a.cc
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <array>
+#include <bit>
+#include <bitset>
+#include <cctype>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <map>
+#include <numeric>
+#include <set>
+#include <string>
+#include <string_view>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using i8 = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+using isize = std::make_signed<size_t>::type;
+using u8 = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+using usize = size_t;
+using f32 = float;
+using f64 = double;
+
+using namespace std::literals;
+
+int main() {
+  std::vector<i64> A(3);
+  std::cin >> A[0] >> A[1] >> A[2];
+  std::sort(A.begin(), A.end());
+  std::cout << (A[2] - A[0]) << std::endl;
+exit:
+  return 0;
+}
